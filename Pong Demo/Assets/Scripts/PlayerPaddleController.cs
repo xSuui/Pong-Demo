a650212c21e4c01cb63afe87370552cd8afd876d
xSuui <samuel2012rogerio@gmail.com>
2023-12-12 18:08:31 -0300

    add move to player

---
+++ Pong Demo/Assets/Scripts/PlayerPaddleController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPaddleController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        // Captura da entrada vertical (seta para cima, seta para baixo, teclas W e S)
+        float moveInput = Input.GetAxis("Vertical");
+
+        // Calcula a nova posição da raquete baseada na entrada e na velocidade
+        Vector3 newPosition = transform.position + Vector3.up * moveInput * speed * Time.deltaTime;
+
+        // Limita a posição vertical da raquete para que ela não saia da tela
+        newPosition.y = Mathf.Clamp(newPosition.y, - 4.5f, 4.5f);
+
+        // Atualiza a posição da raquete
+        transform.position = newPosition;
+    }
+}
